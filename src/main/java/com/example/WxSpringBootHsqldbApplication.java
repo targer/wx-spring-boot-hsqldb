dcc1def5c5438bf48a1c3ee0bb8bb9446c6730de
xj <william.xuj@live.com>
2016-01-07 15:33:20 +0800

    origi commit

---
+++ src/main/java/com/example/WxSpringBootHsqldbApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSpringBootHsqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxSpringBootHsqldbApplication.class, args);
+	}
+}
